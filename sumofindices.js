1ae4d9b85e1984367832f2928c6a1ceb873ac88a
Mahesh Moka <mokamahesh77@gmail.com>
2025-02-24 22:17:06 +0530

    Commiting the indices sum

---
+++ sumofindices.js
@@ -0,0 +1,13 @@
+function printarray(arr){
+    for(let i=0;i<N;i++){
+        let bag=""
+        for(let j=0;j<M;j++){
+        bag+=i+j+" "
+        }
+        console.log(bag)
+    }
+}
+
+let arr=[[1,2],[3,4],[5,6]];
+let N=3,M=2;
+printarray(arr);
