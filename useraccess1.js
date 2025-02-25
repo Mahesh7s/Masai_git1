49dd448eb12ce5465d2f1be9ee30362313ebf66c
Mahesh Moka <mokamahesh77@gmail.com>
2025-02-25 14:06:32 +0530

    Adding this user access message code

---
+++ useraccess1.js
@@ -0,0 +1,17 @@
+function userAccessMessage(user) {
+    return user.role === "admin" 
+        ? user.active 
+            ? "Admin Access Granted!" 
+            : "Admin Access Revoked"
+        : user.role === "user" 
+            ? user.active 
+                ? "User Access Granted!" 
+                : "User Access Revoked"
+            : "Access Denied";
+}
+
+let user1 = { name: "Alice", role: "admin", active: false };
+console.log(userAccessMessage(user1)); 
+
+let user2 = { name: "Bob", role: "user", active: true };
+console.log(userAccessMessage(user2)); 
