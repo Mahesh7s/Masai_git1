107aded0b85da6f1801767a7d5baf12d4e6a4250
Mahesh Moka <mokamahesh77@gmail.com>
2025-03-05 17:36:25 +0530

    commiting global context

---
+++ ss/glb1.js
@@ -0,0 +1,15 @@
+var age=25;
+function displayAge(){
+    console.log(age);
+}
+displayAge();
+function changeAge(){
+    age=30;
+    console.log(age)
+}
+displayAge();
+changeAge();
+//displayAge();
+//console.log(age)
+
+
